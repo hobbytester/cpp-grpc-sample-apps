a2cc034588e6fe849c40d8b482593b5ce0b55bc7
Heikei <hobby.tester.apps@gmail.com>
2023-07-09 20:09:34 +0700

    scripts:add build script and update path
    Signed-off-by: Heikei <hobby.tester.apps@gmail.com>

---
+++ scripts/build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+BUILD_DIR=${SCRIPT_DIR}/../build
+
+rm -rf $BUILD_DIR
+mkdir -p $BUILD_DIR
+
+pushd $BUILD_DIR
+cmake .. && make
+popd
--- scripts/genProto.sh
+++ scripts/genProto.sh
@@ -1,12 +1,13 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-MY_INSTALL_DIR=~/data/97_Hobbies/04_gRPC/local
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+MY_INSTALL_DIR="${SCRIPT_DIR}/../../local"
 PATH="$MY_INSTALL_DIR/bin:$PATH"
 
 PROTO_FILE="greeter.proto"
-
-PROTO_DIR="./proto"
-GEN_DIR="./gen"
+PROTO_DIR="${SCRIPT_DIR}/../proto"
+GEN_DIR="${SCRIPT_DIR}/../gen"
 
 protoc -I=$PROTO_DIR --cpp_out=$GEN_DIR "${PROTO_DIR}/${PROTO_FILE}"
 protoc -I=$PROTO_DIR --grpc_out=$GEN_DIR \
