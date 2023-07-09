f950fff78f1832c9f2d69a0921060c5d9bf2e464
Heikei <hobby.tester.apps@gmail.com>
2023-07-09 20:57:06 +0700

    server:implementation
    Signed-off-by: Heikei <hobby.tester.apps@gmail.com>

--- server.cpp
+++ server.cpp
@@ -1,14 +1,56 @@
+#include <csignal>
 #include <string>
 #include <iostream>
+#include <grpc++/grpc++.h>
+#include "gen/greeter.grpc.pb.h"
+
+std::unique_ptr<grpc::Server> gServer;
 
 void logInfo(std::string message) {
     std::cout << "[INFO] " << message << std::endl;
 }
 
+void SignalHandler(int sigNum) {
+    logInfo("Handle SIGINT");
+    if (gServer != NULL) {
+        logInfo("Server is shutdown!!!");
+        gServer->Shutdown();
+    }
+}
+
+class GreeterServiceImpl final : public Greeter::GreeterService::Service {
+    grpc::Status SayHi(grpc::ServerContext* context, const Greeter::HiRequest* request, Greeter::HiResponse* response) override {
+        logInfo("Serving RPC SayHi: " + request->target_name());
+        response->set_message("Hi there!");
+        return grpc::Status::OK;
+    }
+
+    grpc::Status SayHiAgain(grpc::ServerContext* context, const Greeter::HiRequest* request, Greeter::HiResponse* response) override {
+        logInfo("Serving RPC SayHiAgain: " + request->target_name());
+        response->set_message("Hello there");
+        return grpc::Status::OK;
+    }
+};
+
+void RunServer() {
+    GreeterServiceImpl greeterService;
+
+    grpc::ServerBuilder serverBuilder;
+    std::string server_address{"localhost:50051"};
+    serverBuilder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+    serverBuilder.RegisterService(&greeterService);
+
+    gServer = serverBuilder.BuildAndStart();
+    logInfo("Server listening on " + server_address);
+    gServer->Wait();
+}
+
 int main(int argc, char** argv) {
-    logInfo("Cpp gRPC Server is RUNNING...");
+    logInfo("Cpp gRPC Server App is RUNNING...");
+    signal(SIGINT, SignalHandler);
 
+    RunServer();
 
-    logInfo("Cpp gRPC Server is EXITING...");
+    logInfo("Cpp gRPC Server App is EXITING...");
     return 0;
 }
